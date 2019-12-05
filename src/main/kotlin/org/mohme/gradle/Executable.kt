0d1871b97e5d6ca9a6ebde55539beb28aa8cb9d3
Thomas Mohme <2261413+tmohme@users.noreply.github.com>
2019-12-05 22:32:01 +0100

    add platform detection; refactor path() code further to use Result

--- src/main/kotlin/org/mohme/gradle/Executable.kt
+++ src/main/kotlin/org/mohme/gradle/Executable.kt
@@ -1,6 +1,7 @@
 package org.mohme.gradle
 
 import com.github.kittinunf.result.Result
+import com.github.kittinunf.result.Result.Companion.success
 import com.github.kittinunf.result.flatMap
 import com.github.kittinunf.result.map
 import java.io.File
@@ -16,30 +17,44 @@ sealed class Executable : Serializable {
 
     @Suppress("ClassNaming")
     data class V_0_19_0(
-            private val version: String = "0.19.0",
-            private val artifactName: String = "binary-for-mac-64-bit.gz"
+            private val version: String = "0.19.0"
     ) : Executable() {
-        override fun path(logger: Logger, baseDir: File): Result<Path, Exception> {
-            // TODO extract shareable code
-            // TODO implement platform distinction
-            val url = URL("${githubDownloadBasePath}/${version}/${artifactName}")
-            val targetFile = baseDir
-                    .resolve("gradle-elm")
-                    .resolve(version)
-                    .resolve(artifactName)
-
-            val fileResult =
-                    if (targetFile.exists()) Result.of { targetFile }
-                    else Downloader(logger).fetch(url, targetFile)
-
-            return fileResult
-                    .flatMap { downloaded -> Unpacker.unpack(downloaded) }
-                    .map { unpacked -> unpacked.apply { setExecutable(true) } }
-                    .map { file -> file.toPath() }
-        }
+        override fun path(logger: Logger, baseDir: File): Result<Path, Exception> =
+                // TODO extract shareable code
+                platformCode
+                        .map { artifactNameTemplate.replace("\$platformCode", it) }
+                        .map { artifactName ->
+                            Pair(artifactName, URL("${githubDownloadBasePath}/${version}/$artifactName"))
+                        }
+                        .map { (artifactName, url) ->
+                            val targetFile = baseDir
+                                    .resolve("gradle-elm")
+                                    .resolve(version)
+                                    .resolve(artifactName)
+                            Pair(targetFile, url)
+                        }
+                        .flatMap { (targetFile, url) ->
+                            if (targetFile.exists()) success(targetFile)
+                            else Downloader(logger).fetch(url, targetFile)
+                        }
+                        .flatMap { downloaded -> Unpacker.unpack(downloaded) }
+                        .map { unpacked -> unpacked.apply { setExecutable(true) } }
+                        .map { file -> file.toPath() }
     }
 
     data class Provided(val name: String = "elm") : Executable() {
         override fun path(logger: Logger, baseDir: File) = Result.of<Path, Exception> { File(name).toPath() }
+    }
+
+    companion object {
+        private const val artifactNameTemplate = "binary-for-\$platformCode-64-bit.gz"
+        private val os = System.getProperty("os.name").toLowerCase()
+        val platformCode = when {
+            os.contains("nux") -> success("linux")
+            os.contains("mac") -> success("mac")
+            os.contains("win") -> success("windows")
+            else ->
+                error(IllegalStateException("Unable to determine platform (determining the correct file to download)"))
+        }
     }
 }
